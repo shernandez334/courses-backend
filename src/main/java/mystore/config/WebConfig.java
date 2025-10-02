767a0d91760444af34083ea80ed8085c6d7200d0
shernandez334 <santiagoso1998@gmail.com>
2025-10-02 19:25:10 +0200

    Updated cors to accept Netlify 3

--- src/main/java/mystore/config/WebConfig.java
+++ src/main/java/mystore/config/WebConfig.java
@@ -14,11 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://127.0.0.1:5500",
-                                "http://localhost:5500",
-                                "https://tangerine-pony-f53ece.netlify.app"
-                        )
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
